bf99a5568827f6aa89005741385bd120f139879c
KARAN1234RAJ <36782443+KARAN1234RAJ@users.noreply.github.com>
2024-06-28 11:34:55 +0530

    Update p.js

--- Backent-07/expressWithMD/src/controllers/p.js
+++ Backent-07/expressWithMD/src/controllers/p.js
@@ -1,9 +1,1 @@
-const getAllProd =(req, res) => {
-       Product.find()
-         .then((data) => {
-           res.json({ status: "success", total: data.length, result: data });
-         })
-         .catch((err) => {
-           res.json({ status: "failed", error: err.message });
-         });
-     }
+
